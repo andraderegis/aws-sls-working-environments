5e6a8325becdbef336ff144a5865bd05a76d8385
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-07-23 16:28:27 -0300

    feat: :sparkles: finished handler

--- handler.js
+++ handler.js
@@ -1,7 +1,12 @@
 'use strict';
+const { DynamoDB } = require('aws-sdk');
 const axios = require('axios');
+const cheerio = require('cheerio');
+const uuid = require('uuid');
 
 const { API_COMMIT_MESSAGES_URL, DB_TABLE_NAME, NODE_ENV } = require('./config/settings');
+
+const dynamoDB = new DynamoDB.DocumentClient();
 
 class Handler {
   static async main(event) {
@@ -9,7 +14,21 @@ class Handler {
 
     const { data } = await axios.get(API_COMMIT_MESSAGES_URL);
 
-    console.log({ data });
+    const $ = cheerio.load(data);
+    const [commitMessage] = $('#content').text().trim().split('\n');
+
+    console.log({ commitMessage });
+
+    const params = {
+      TableName: DB_TABLE_NAME,
+      Item: {
+        commitMessage,
+        id: uuid.v4(),
+        createdAt: new Date().toISOString()
+      }
+    }
+
+    await dynamoDB.put(params).promise();
 
     return {
       statusCode: 200
